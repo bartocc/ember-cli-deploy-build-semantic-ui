1788d6c4323fd3a80e8319fed56fe21a7068a806
Julien Palmas <julien.palmas@gmail.com>
2017-05-24 14:52:36 +0200

    add gulp and start deploy config

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* eslint-env node */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+var gulp = require('gulp');
+
+// TODO: fetch <my-app> from the ember-cli app consuming
+// ember-cli-deploy-build-semantic-ui
+require('<my-app>/vendor/semantic-ui/gulpfile');
+
 module.exports = {
-  name: 'ember-cli-deploy-build-semantic-ui'
+  name: 'ember-cli-deploy-build-semantic-ui',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      willBuild: function(context) {
+        gulp.start('build');
+      },
+    });
+
+    return new DeployPlugin();
+  }
 };
